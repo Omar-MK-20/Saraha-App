845deb3c3df65be918ddb6b348499f796516b724
Omar-MK-20 <omar.mk.201@gmail.com>
2026-04-01 17:20:52 +0200

    feat: implement email sending functionality with HTML template using Nodemailer

--- src/util/Nodemailer/mail.config.js
+++ src/util/Nodemailer/mail.config.js
@@ -1,5 +1,8 @@
+import fs from "node:fs/promises";
+import path from "node:path";
 import nodemailer from "nodemailer";
 import { GOOGLE_APP_PASSWORD, GOOGLE_NODEMAILER_USER } from "../../../config/app.config.js";
+import { ResponseError } from "../Res/ResponseError.js";
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -10,20 +13,28 @@ const transporter = nodemailer.createTransport({
 });
 
 
-try
-{
-    const info = await transporter.sendMail({
-        from: '"Example Team" <team@example.com>', // sender address
-        to: "omar200ram@gmail.com", // list of recipients
-        subject: "Hello", // subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // HTML body
-    });
+const htmlFilePath = path.resolve("src/util/Nodemailer/mailTemplate.html");
+const readHtmlFile = await fs.readFile(htmlFilePath, "utf-8");
 
-    console.log("Message sent: %s", info.messageId);
-    // Preview URL is only available when using an Ethereal test account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-} catch (err)
+
+export async function sendMail({ email })
 {
-    console.error("Error while sending mail:", err);
+    try
+    {
+        const info = await transporter.sendMail({
+            from: '"Example Team" <team@example.com>', // sender address
+            to: email, // list of recipients
+            subject: "Hello", // subject line
+            text: "Hello world?", // plain text body
+            html: readHtmlFile, // HTML body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Preview URL is only available when using an Ethereal test account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    } catch (err)
+    {
+        console.error("Error while sending mail:", err);
+        throw new ResponseError("Error while sending mail", 500, err);
+    }
 }
