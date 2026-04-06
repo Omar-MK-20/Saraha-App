627078ace152680959b651d5da161e92e308c150
Omar-MK-20 <omar.mk.201@gmail.com>
2026-04-06 18:11:59 +0200

    feat: add file validation schema to ValidationMiddleware for enhanced file upload validation

--- src/util/Middleware/ValidationMiddleware.js
+++ src/util/Middleware/ValidationMiddleware.js
@@ -1,6 +1,6 @@
 import joi from "joi";
-import { ContentError } from "../Res/ResponseError.js";
 import { UserGender } from "../Enums/user.enums.js";
+import { ContentError } from "../Res/ResponseError.js";
 
 /**
  * @param {{body?: import("joi").ObjectSchema}} schema 
@@ -78,4 +78,15 @@ export const ValidationType = {
 
 
 
+export const FileValidationType = joi.object({
+    fieldname: joi.string().required(),
+    originalname: joi.string().required(),
+    encoding: joi.string().required(),
+    mimetype: joi.string().required(),
+    finalPath: joi.string().required(),
+    destination: joi.string().required(),
+    filename: joi.string().required(),
+    path: joi.string().required(),
+    size: joi.number().required(),
+}).messages({ "any.required": "uploading media is required" })
 
