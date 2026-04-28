42920834af052d4a1b0f15d053ac9ceb9f66bff7
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2026-04-28 03:38:40 +0300

    feat: set default expiration value in set function and ensure consistent function structure

--- src/DB/redis.service.js
+++ src/DB/redis.service.js
@@ -1,6 +1,34 @@
 import { redisClient } from "./redis.connection.js";
 
-export async function set({ key, value, exType = "EX", exValue })
+export async function set({ key, value, exType = "EX", exValue = 120 })
 {
     return await redisClient.set(key, value, { expiration: { type: exType, value: exValue } });
+}
+
+export async function get(key)
+{
+    return await redisClient.get(key);
+}
+
+export async function ttl(key)
+{
+    return await redisClient.ttl(key);
+}
+
+export async function exists(key)
+{
+    return await redisClient.exists(key);
+}
+
+export async function del(keys)
+{
+    return await redisClient.del(keys);
+}
+
+export async function update(key, value)
+{
+    if (!(exists(key))) return 0;
+
+    await redisClient.set(key, value);
+    return 1;
 }
